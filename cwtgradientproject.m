2dd065c6d927d3ee70ba74272ea258a9786fd2ec
Nur Rochman Muhammad <nurrochmanmuhammad@gmail.com>
2020-06-10 08:57:12 +0700

    Delete cwtgradientproject.m

--- cwtgradientproject.m
+++
@@ -1,7 +0,0 @@
-[seismic10,text_header,binary_header]=read_segy_file_legacy('C:\Users\R\Documents\MATLAB\model-hrs-tomo0-gli.sgy');
-[seismic15,text_header,binary_header]=read_segy_file_legacy('C:\Users\R\Documents\MATLAB\model-hrs-tomo0-gli.sgy');
-[seismic20,text_header,binary_header]=read_segy_file_legacy('C:\Users\R\Documents\MATLAB\model-hrs-tomo0-gli.sgy');
-[seismic25,text_header,binary_header]=read_segy_file_legacy('C:\Users\R\Documents\MATLAB\model-hrs-tomo0-gli.sgy');
-[seismic30,text_header,binary_header]=read_segy_file_legacy('C:\Users\R\Documents\MATLAB\model-hrs-tomo0-gli.sgy');
-[seismic35,text_header,binary_header]=read_segy_file_legacy('C:\Users\R\Documents\MATLAB\model-hrs-tomo0-gli.sgy');
-[seismic40,text_header,binary_header]=read_segy_file_legacy('C:\Users\R\Documents\MATLAB\model-hrs-tomo0-gli.sgy');
